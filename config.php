899b660f56982a0fddebaa1d0c16d6e8c157698a
Giovani <giovanimouradev@gmail.com>
2021-04-29 12:49:50 -0300

    config,index,e as pasta assets usuaso estrura mvc

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+require 'environment.php';
+
+$config = array();
+
+if(ENVIRONMENT == 'development'){
+    DEFINE("BASE_URL","http://www.sysazul.com.br/");
+    $config["dbname"] ="contazul";
+    $config["host"] ="localhost";
+    $config["dbuser"] ="root";
+    $config["dbpass"] ="";
+
+}else{
+//servidor
+
+        $config["dbname"] ="contazul";
+        $config["host"] ="localhost";
+        $config["dbuser"] ="root";
+        $config["dbpass"] ="";
+    
+
+}
+
+try{
+
+    global $pdo;
+    $pdo = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], 
+    $config['dbuser'], $config['dbpass']);
+
+}catch(PDOexception $x){
+    echo "Falha na conexão com banco ".$x->getMessage();
+    exit;
+
+}
